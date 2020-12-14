391dac0227aa38aa4335031bc1c926a8b7f752ed
Julian Heredia <julian00hs@gmail.com>
2020-12-13 22:25:26 -0500

    agregada nueva funcion de API y mensaje cambiado en el carrusel de imagenes

--- app.py
+++ app.py
@@ -31,6 +31,19 @@ def api():
     else:
         return "No ha funcionado"
 
+@app.route('/beer/<id>')
+def beer(id):
+    url = 'https://api.punkapi.com/v2/beers/'
+    paramss = id
+    res = requests.request("GET", url, params = paramss)
+    if res.status_code == 200:
+        body = res.json()
+        body_list = body[int(id)]
+        body_string = str(body_list)
+        return body_string
+    else:
+        return "No ha funcionado"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
